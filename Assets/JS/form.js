565f166da5d1d64a31cc707d0926e002236eb074
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-08-15 20:37:45 +0300

    upd

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -1,3 +1,46 @@
+/* phone mask */
+
+
+/* validate */
+const validate = new JustValidate('#form', {
+    validateBeforeSubmitting: true,
+});
+validate
+  .addField('#user_name', [
+    {
+      rule: 'required',
+      errorMessage: 'Поле Имя обязательно к заполнению',
+    },
+    {
+      rule: 'customRegexp',
+      value: /^[А-Я]+$/i,      
+      errorMessage: 'Поле Имя должно содержать только кириллицу',
+    },    
+    {
+      rule: 'minLength',
+      value: 3,      
+      errorMessage: 'Поле Имя должно иметь минимум 3 символа',
+    },    
+  ])
+  .addField('#phone_number', [
+    {
+      rule: 'required',
+      errorMessage: 'Поле Телефон обязательно к заполнению',
+    },    
+    {
+        rule: 'customRegexp',
+        value: /[0-9]/,      
+        errorMessage: 'Поле Телефон должно содержать только цифры',
+    },
+    /* {
+      rule: 'minLength',
+      value: 11,      
+      errorMessage: 'Поле Телефон должно иметь минимум 11 символов',
+    } */
+  ]);
+
+
+/* submit form */
 const token = "6688184188:AAGzlkIWoBx9zuXSh71W6Jl_5Woat_RzxlU"
 const chat_id = "-920280170"
 const url = `https://api.telegram.org/bot${token}/sendMessage`
@@ -5,6 +48,7 @@ const url = `https://api.telegram.org/bot${token}/sendMessage`
 document.getElementById("form").addEventListener("submit", function (e) {
     e.preventDefault();
 
+    if(validate.isValid) {
     let message = `<b>Заявка с сайта!</b>\n`;
     message += `<b>Отправитель: </b> ${this.name.value}\n`;
     message += `<b>Номер телефона: </b> ${this.number.value}`;
@@ -23,5 +67,6 @@ document.getElementById("form").addEventListener("submit", function (e) {
     })
     .finally(() => {
         console.log('Конец');
-    })
+    })            
+    }
 })
